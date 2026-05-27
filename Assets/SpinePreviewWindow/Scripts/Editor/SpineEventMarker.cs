626825bc703cc17d9184468fcc68a0e7c904adc1
son24tuoi <son23tuoi@gmail.com>
2026-05-27 22:23:07 +0700

    + Add namespace event marker class

--- Assets/SpinePreviewWindow/Scripts/Editor/SpineEventMarker.cs
+++ Assets/SpinePreviewWindow/Scripts/Editor/SpineEventMarker.cs
@@ -2,25 +2,29 @@
 
 using System;
 
-public struct SpineEventMarker : IComparable<SpineEventMarker>
+namespace One.Editor.Spine
 {
-    public string name;
-    public float time;
-    public bool isCustom;
-
-    public SpineEventMarker(string name, float time, bool isCustom = false)
+    public struct SpineEventMarker : IComparable<SpineEventMarker>
     {
-        this.name = name;
-        this.time = time;
-        this.isCustom = isCustom;
+        public string name;
+        public float time;
+        public bool isCustom;
+
+        public SpineEventMarker(string name, float time, bool isCustom = false)
+        {
+            this.name = name;
+            this.time = time;
+            this.isCustom = isCustom;
+        }
+
+        public int CompareTo(SpineEventMarker other)
+        {
+            return time.CompareTo(other.time);
+        }
+
+        public readonly bool Equal(SpineEventMarker other) => name == other.name && time == other.time;
     }
 
-    public int CompareTo(SpineEventMarker other)
-    {
-        return time.CompareTo(other.time);
-    }
-
-    public readonly bool Equal(SpineEventMarker other) => name == other.name && time == other.time;
-}
 
 #endif
+}
